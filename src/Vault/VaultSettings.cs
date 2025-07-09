de6ed0d30d77e235ec4597712a5f5d4d078072c9
rick <rick@rickdot.net>
2025-07-08 20:54:24 -0500

    RIP htmx, it really was fun

--- src/Vault/VaultSettings.cs
+++ src/Vault/VaultSettings.cs
@@ -3,8 +3,8 @@ namespace Vault;
 public record VaultSettings
 {
     public string NatsUrl { get; init; } = "nats://localhost:4222";
+    public string IssuerKey { get; init; } = "ACBEED26BZNEL3EKFSK4UKITTJOQZPP4HJBAGI4VNXUCDKPKNTNN2NUA";
+    public string IssuerSeed { get; init; } = "SAAAE2JMCZNPDPRS3AKEAW6UZFNM3BBFETKF2SYBF6G37ZWYINHP5LMP6M";
     public string? NatsUser { get; init; }
     public string? NatsPass { get; init; }
-    public string? IssuerKey { get; init; }
-    public string? IssuerSeed { get; init; }
 }
